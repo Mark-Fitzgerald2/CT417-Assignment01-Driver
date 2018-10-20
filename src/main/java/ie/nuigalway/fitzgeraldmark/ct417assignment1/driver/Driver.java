d0b149d419967fa24c74de25a730798c5ed9232e
I342039 <I342039@DUBN34198882A.emea.global.corp.sap>
2018-10-20 13:11:53 +0100

    Created two courses and printed information to check they are working

--- src/main/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/driver/Driver.java
+++ src/main/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/driver/Driver.java
@@ -5,10 +5,24 @@
  */
 package ie.nuigalway.fitzgeraldmark.ct417assignment1.driver;
 
+import ie.nuigalway.fitzgeraldmark.ct417assignment1.*;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+
 /**
  *
  * @author Mark Fitzgerald, NUI Galway
  */
 public class Driver {
-    
+    public static void main(String[] args){
+        Course ece = new Course("Electronic and Computer Engineering (ECE)", DateTime.parse("10/09/2018", DateTimeFormat.forPattern("dd/MM/yyyy")), DateTime.parse("10/05/2019", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        Course csit = new Course("Computer Science and Information Technology (CS&IT)", DateTime.parse("10/09/2018", DateTimeFormat.forPattern("dd/MM/yyyy")), DateTime.parse("10/05/2019", DateTimeFormat.forPattern("dd/MM/yyyy")));
+        
+        System.out.println(ece.getName());
+        System.out.println(ece.getStartDate());
+        System.out.println(ece.getEndDate());
+        System.out.println(csit.getName());
+        System.out.println(csit.getStartDate());
+        System.out.println(csit.getEndDate());
+    }
 }
