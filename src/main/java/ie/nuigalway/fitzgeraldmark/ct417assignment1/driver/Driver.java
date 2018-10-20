f09458576a9726186b05b32feb2e64431ae04c05
I342039 <I342039@DUBN34198882A.emea.global.corp.sap>
2018-10-20 13:05:55 +0100

    Add Jitpack dependency to POM and created driver class

---
+++ src/main/java/ie/nuigalway/fitzgeraldmark/ct417assignment1/driver/Driver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.nuigalway.fitzgeraldmark.ct417assignment1.driver;
+
+/**
+ *
+ * @author Mark Fitzgerald, NUI Galway
+ */
+public class Driver {
+    
+}
